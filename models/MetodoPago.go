6a9d041380fa2ac07893c4fda9e0a03f5c4972c2
Bryan Luis <baluisto96@gmail.com>
2025-08-31 08:18:44 -0500

    Update MetodoPago ID type and field size

--- models/MetodoPago.go
+++ models/MetodoPago.go
@@ -3,9 +3,9 @@ package models
 import "github.com/beego/beego/v2/client/orm"
 
 type MetodoPago struct {
-	PK_ID_METODO_PAGO int    `orm:"column(pk_id_metodo_pago);pk;auto" json:"metodoPagoId"`
-	TIPO              string `orm:"column(tipo);size(50)" json:"tipo"`
-	DETALLE           string `orm:"column(detalle);type(text);null" json:"detalle"`
+        PK_ID_METODO_PAGO int64  `orm:"column(pk_id_metodo_pago);pk;auto" json:"metodoPagoId"`
+        TIPO              string `orm:"column(tipo);type(text)" json:"tipo"`
+        DETALLE           string `orm:"column(detalle);type(text);null" json:"detalle"`
 }
 
 func (m *MetodoPago) TableName() string {
