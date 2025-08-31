5841fb099b253d476e104594c165ca6e48bd7433
Bryan Luis <baluisto96@gmail.com>
2025-08-31 16:35:35 -0500

    Mark product ID as part of order detail primary key

--- models/DetallePedido.go
+++ models/DetallePedido.go
@@ -4,7 +4,7 @@ import "github.com/beego/beego/v2/client/orm"
 
 type DetallePedido struct {
 	PKIDPedido   int64 `orm:"column(pk_id_pedido);pk" json:"pedidoId"`
-	PKIDProducto int64 `orm:"column(pk_id_producto)" json:"productoId"` // part of composite PK
+	PKIDProducto int64 `orm:"column(pk_id_producto);pk" json:"productoId"` // part of composite PK
 	Precio       int64 `orm:"column(precio);type(bigint)" json:"precio"`
 	Cantidad     int   `orm:"column(cantidad)" json:"cantidad"`
 }
