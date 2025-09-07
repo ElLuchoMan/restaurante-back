2ea1f3a13fc50c3fa287d8d65107ff77028847c1
Bryan Luis <baluisto96@gmail.com>
2025-09-07 17:36:25 -0500

    Updated: tests

---
+++ controllers/incidencia_delete_notfound_hook_test.go
@@ -0,0 +1,35 @@
+package controllers
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/beego/beego/v2/client/orm"
+	"github.com/beego/beego/v2/server/web/context"
+)
+
+// fake ormer to force Delete not found path
+type fakeIncidOrmNotFound struct{}
+func (fakeIncidOrmNotFound) QueryTable(interface{}) orm.QuerySeter { return nil }
+func (fakeIncidOrmNotFound) Insert(interface{}) (int64, error) { return 0, nil }
+func (fakeIncidOrmNotFound) Read(interface{}, ...string) error { return nil }
+func (fakeIncidOrmNotFound) Update(interface{}, ...string) (int64, error) { return 0, nil }
+func (fakeIncidOrmNotFound) Delete(interface{}, ...string) (int64, error) { return 0, orm.ErrNoRows }
+
+func TestIncidenciaDeleteNotFound_Hook(t *testing.T) {
+    orig := incidenciaOrmNew
+    incidenciaOrmNew = func() incidenciaOrmer { return fakeIncidOrmNotFound{} }
+    t.Cleanup(func(){ incidenciaOrmNew = orig })
+
+    r := httptest.NewRequest(http.MethodDelete, "/incidencias?id=1", nil)
+    w := httptest.NewRecorder(); ctx := context.NewContext(); ctx.Reset(w, r)
+    c := IncidenciaController{}; c.Ctx = ctx; c.Data = make(map[interface{}]interface{})
+
+    c.Delete()
+    if w.Code != http.StatusOK {
+        t.Fatalf("expected 200 wrapper, got %d", w.Code)
+    }
+}
+
+
